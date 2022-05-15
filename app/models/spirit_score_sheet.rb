b55e5e6e321a37872222d12e4dae03d5281ef813
Anbranin <keichelman@gmail.com>
2022-05-15 07:14:13 -0400

    flesh out and fix spirit score model

--- app/models/spirit_score_sheet.rb
+++ app/models/spirit_score_sheet.rb
@@ -1,2 +1,8 @@
-class SpiritScoreSheet < ApplicationController
+class SpiritScoreSheet < ApplicationRecord
+  belongs_to :team
+
+  belongs_to :opponent, class_name: 'Team', foreign_key: :opponent_id
+
+  NUMBER_FIELDS = %i[rules_knowledge_and_use fouls_and_body_contact
+  fair_mindedness positive_attitude_and_self_control communication]
 end
