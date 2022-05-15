ebf66489287d40bebd4b9f1e3e61f15261a7ac0b
Anbranin <keichelman@gmail.com>
2022-05-15 07:14:25 -0400

    add some stuff to team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,17 @@ class Team < ApplicationRecord
 
   validates :name, uniqueness: { scope: :division }
 
+  has_many :spirit_score_sheets
+
+  has_many :spirit_scores, class_name: 'SpiritScoreSheet', foreign_key: :opponent_id
+
   def description
-    "#{team.name} - #{division.name}"
+    "#{name} - #{division.name}"
+  end
+
+  def average_spirit_score
+    SpiritScoreSheet::NUMBER_FIELDS.map do |field|
+      spirit_scores.map(&field)
+    end.flatten.sum
   end
 end
