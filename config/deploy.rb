3bcc15a6c36a14e7f491465fbdf93c9b03dd33ba
Anbranin <keichelman@gmail.com>
2022-05-16 22:13:11 -0400

    don't overwrite logfiles

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-append :linked_dirs, '.bundle'
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage", '.bundle'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
