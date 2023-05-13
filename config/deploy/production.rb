766ddddbcdc7904f5e22ecbff26f8c5b9ae6add2
Anbranin <keichelman@gmail.com>
2023-05-13 13:23:26 -0400

    use the right place

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'http://pvispiritscores.com/', user: 'karin', roles: %w{app db web}
+server 'pvispiritscores.com', user: 'karin', roles: %w{app db web}
 
 # role-based syntax
 # ==================
